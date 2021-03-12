a94dd9ea848d3d59575e7c2df5e25580764c0a46
Graeme T. Lloyd <graemetlloyd@gmail.com>
2021-03-12 12:00:54 +0000

    Added biogeography folder.

---
+++ Biogeography/All_taxa/BioGeoBEARS/BioGeoBEARS_ALL.R
@@ -0,0 +1,241 @@
+library(GenSA)    # GenSA is better than optimx (although somewhat slower)
+library(FD)       # for FD::maxent() (make sure this is up-to-date)
+library(snow)     # (if you want to use multicore functionality; some systems/R versions prefer library(parallel), try either)
+library(parallel)
+library(rexpokit)
+library(cladoRcpp)
+library(BioGeoBEARS)
+rm(list=ls())
+
+## complete primate reanalysis with DEC+J 
+setwd("Biogeography/All_taxa/")
+trfn = "BioGeoBEARS/Primate_All_noZero.tre"
+tr <- read.tree(trfn)
+geogfn = "allTaxa.phy"
+tipranges = getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+
+max_range_size=2
+
+
+### Run DEC
+
+# Intitialize a default model (DEC model)
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+
+# Give BioGeoBEARS the location of the phylogeny Newick file
+BioGeoBEARS_run_object$trfn = trfn
+
+# Give BioGeoBEARS the location of the geography text file
+BioGeoBEARS_run_object$geogfn = geogfn
+
+# Input the maximum range size
+BioGeoBEARS_run_object$max_range_size = max_range_size
+
+BioGeoBEARS_run_object$min_branchlength = 0.000001    # Min to treat tip as a direct ancestor (no speciation event)
+BioGeoBEARS_run_object$include_null_range = TRUE    # set to FALSE for e.g. DEC* model, DEC*+J, etc.
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$on_NaN_error = -1e50    # returns very low lnL if parameters produce NaN error (underflow check)
+BioGeoBEARS_run_object$speedup = TRUE          # shorcuts to speed ML search; use FALSE if worried (e.g. >3 params)
+BioGeoBEARS_run_object$use_optimx = "GenSA"    # if FALSE, use optim() instead of optimx()
+BioGeoBEARS_run_object$num_cores_to_use = 8
+BioGeoBEARS_run_object$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# For a slow analysis, run once, then set runslow=FALSE to just 
+# load the saved result.
+runslow = TRUE
+resfn = "Primates_unconstrained_v1.Rdata"
+if (runslow)
+{
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  resDEC = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDEC = res
+}
+
+runslow = FALSE
+resfn = "Primates_unconstrained_v1.Rdata"
+if (runslow)
+{
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  resDEC = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDEC = res
+}
+
+# Probabilities of states/ranges at each node
+# To get the probabilities of each state/range at each node:
+# What you want, if "res" is your results object, is:
+resDEC$ML_marginal_prob_each_state_at_branch_top_AT_node
+
+areas = getareas_from_tipranges_object(tipranges)
+areas <- c("NAM","NEO","EUR","AFR", "MAD")
+# This is the list of states/ranges, where each state/range
+# is a list of areas, counting from 0
+states_list_0based = rcpp_areas_list_to_states_list(areas=areas, maxareas=max_range_size, include_null_range=TRUE)
+
+# Make the list of ranges
+ranges_list = NULL
+for (i in 1:length(states_list_0based))
+{    
+  if ( (length(states_list_0based[[i]]) == 1) && (is.na(states_list_0based[[i]])) )
+  {
+    tmprange = "_"
+  } else {
+    tmprange = paste(areas[states_list_0based[[i]]+1], collapse="")
+  }
+  ranges_list = c(ranges_list, tmprange)
+}
+
+# Make the node numbers the row names
+# Make the range_list the column names
+range_probabilities = as.data.frame(resDEC$ML_marginal_prob_each_state_at_branch_top_AT_node)
+row.names(range_probabilities) = trtable$node
+names(range_probabilities) = ranges_list
+range_probabilities
+
+range_probabilities<-range_probabilities[-(1:length(tr$tip.label)),]
+head(range_probabilities)
+# Write the table to a tab-delimited text file (for Excel etc.)
+write.table(range_probabilities[-(1:length(tr$tip.label)),]
+            , file="DEC_range_probabilities_biogeobears.txt", quote=FALSE, sep="\t")
+
+tr$node.label <- unique(tr$edge[,1])
+plot(tr, show.tip.label = F)
+nodelabels(cex=0.4)
+write.tree(tr, "DEC_range_labels_biogeobears.tre")
+tr$node.label<-colnames(range_probabilities)[apply(range_probabilities, 1, which.max)]
+write.tree(tr, "DEC_range_biogeobears.tre")
+
+
+# Run DEC+J
+#######################################################
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+BioGeoBEARS_run_object$trfn = trfn
+BioGeoBEARS_run_object$geogfn = geogfn
+BioGeoBEARS_run_object$max_range_size = max_range_size
+BioGeoBEARS_run_object$min_branchlength = 0.000001    # Min to treat tip as a direct ancestor (no speciation event)
+BioGeoBEARS_run_object$include_null_range = TRUE    # set to FALSE for e.g. DEC* model, DEC*+J, etc.
+
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$on_NaN_error = -1e50    # returns very low lnL if parameters produce NaN error (underflow check)
+BioGeoBEARS_run_object$speedup = TRUE          # shorcuts to speed ML search; use FALSE if worried (e.g. >3 params)
+BioGeoBEARS_run_object$use_optimx = "GenSA"    # if FALSE, use optim() instead of optimx()
+BioGeoBEARS_run_object$num_cores_to_use = 1
+BioGeoBEARS_run_object$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DEC+J model
+# Get the ML parameter values from the 2-parameter nested model
+# (this will ensure that the 3-parameter model always does at least as good)
+dstart = resDEC$outputs@params_table["d","est"]
+estart = resDEC$outputs@params_table["e","est"]
+jstart = 0.0001
+
+# Input starting values for d, e
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["d","init"] = dstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["d","est"] = dstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["e","init"] = estart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["e","est"] = estart
+
+# Add j as a free parameter
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","init"] = jstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","est"] = jstart
+
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+resfn = "Primates_DEC+J_v1.Rdata"
+runslow = TRUE
+if (runslow)
+{
+  
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  
+  resDECj = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDECj = res
+}
+
+runslow = FALSE
+if (runslow)
+{
+  
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  
+  resDECj = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDECj = res
+}
+
+# Probabilities of states/ranges at each node
+# To get the probabilities of each state/range at each node:
+# What you want, if "res" is your results object, is:
+resDECj$ML_marginal_prob_each_state_at_branch_top_AT_node
+
+areas = getareas_from_tipranges_object(tipranges)
+areas <- c("NAM","NEO","EUR","AFR", "MAD")
+# This is the list of states/ranges, where each state/range
+# is a list of areas, counting from 0
+states_list_0based = rcpp_areas_list_to_states_list(areas=areas, maxareas=max_range_size, include_null_range=TRUE)
+
+# Make the list of ranges
+ranges_list = NULL
+for (i in 1:length(states_list_0based))
+{    
+  if ( (length(states_list_0based[[i]]) == 1) && (is.na(states_list_0based[[i]])) )
+  {
+    tmprange = "_"
+  } else {
+    tmprange = paste(areas[states_list_0based[[i]]+1], collapse="")
+  }
+  ranges_list = c(ranges_list, tmprange)
+}
+
+# Make the node numbers the row names
+# Make the range_list the column names
+range_probabilities = as.data.frame(resDECj$ML_marginal_prob_each_state_at_branch_top_AT_node)
+row.names(range_probabilities) = trtable$node
+names(range_probabilities) = ranges_list
+range_probabilities
+
+range_probabilities<-range_probabilities[-(1:length(tr$tip.label)),]
+head(range_probabilities)
+# Write the table to a tab-delimited text file (for Excel etc.)
+write.table(range_probabilities[-(1:length(tr$tip.label)),]
+            , file="DECJ_range_probabilities_biogeobears.txt", quote=FALSE, sep="\t")
+
+tr$node.label <- unique(tr$edge[,1])
+plot(tr, show.tip.label = F)
+nodelabels(cex=0.4)
+write.tree(tr, "DECJ_range_labels_biogeobears.tre")
+tr$node.label<-colnames(range_probabilities)[apply(range_probabilities, 1, which.max)]
+write.tree(tr, "DECJ_range_biogeobears.tre")
